7e41c620026961d0d93941e54d651639a8eb23ca
shin <cu7898@gmail.com>
2020-05-26 23:43:31 +0900

    TestF #1 multiply 테스트

--- Codewars_8kyu/N001N010/N001Multiply/test_multiply.py
+++ Codewars_8kyu/N001N010/N001Multiply/test_multiply.py
@@ -4,9 +4,13 @@ from N001N010.N001Multiply import multiply
 
 class TestTest(unittest.TestCase):
 
-    def testAdd(self):
+    def test_add_1(self):
         result = multiply.multiply(2, 4)
         self.assertEqual(result, 8)
+
+    def test_add_2(self):
+        result = multiply.multiply(1, 4)
+        self.assertEqual(result, 4)
 
 
 if __name__ == '__main__':
